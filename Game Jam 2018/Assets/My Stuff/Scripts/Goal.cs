075426f264e71763fc7fdcf1fcfa6905da588f09
Thomas Simon <thomas.simon@btopenworld.com>
2018-11-09 18:34:19 +0000

    Score

--- Game Jam 2018/Assets/My Stuff/Scripts/Goal.cs
+++ Game Jam 2018/Assets/My Stuff/Scripts/Goal.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour 
 {
-	protected int score;
-
-	void Start() 
-	{
-		score = 0;
-	}
+	public static int score = 0;
 
 	void OnTriggerEnter(Collider collider)
 	{
 		score += 1;
 		Destroy(gameObject);
 		Spawner.goalSpawned = false;
+		Debug.Log(score);
 	}
 }
