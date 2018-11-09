3262f4430012d2fee9f23b3d29550aaa8f2a1512
Thomas Simon <thomas.simon@btopenworld.com>
2018-11-09 22:58:16 +0000

    Timer

---
+++ Game Jam 2018/Assets/My Stuff/Scripts/GameEnd.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour
+ {
+	[SerializeField] protected Text timerText;
+	[SerializeField] protected float timerSize;
+	protected float countdown;
+
+	void Start() 
+	{
+		countdown = timerSize;
+	}
+	
+	void Update() 
+	{
+		countdown -= Time.deltaTime;
+		countdown = Mathf.Round(countdown * 100.0f) / 100.0f;
+
+		timerText.text = "Countdown: " + countdown;
+	}
+}
