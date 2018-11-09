7635f8e63e5b227b3f546a70ed8dd1830f1f3c12
Thomas Simon <thomas.simon@btopenworld.com>
2018-11-09 15:25:27 +0000

    Horizontal movement done

--- Game Jam 2018/Assets/My Stuff/Scripts/WindBlockMovement.cs
+++ Game Jam 2018/Assets/My Stuff/Scripts/WindBlockMovement.cs
@@ -1,37 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
+
 
 public class WindBlockMovement : MonoBehaviour 
 {
-	[SerializeField] protected bool isHorizontal;
 	[SerializeField] protected float movementRange;
+	[SerializeField] protected bool isHorizontal;
+
+	protected float maxX;
+	protected float maxZ;
+	protected float minX;
+	protected float minZ;
 
 	float distance = 28.0f;
 
 	void Start()
-	 {
-		
-	}
-	
-	void Update() 
 	{
-		
+		maxX = transform.position.x + movementRange;
+		minX = transform.position.x - movementRange;
+
+		maxZ = transform.position.z + movementRange;
+		minZ = transform.position.z - movementRange;
 	}
 
 	void OnMouseDrag()
 	{
 		Vector3 mousePos;
-		//Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
-
-		if (isHorizontal == true)
-			mousePos = new Vector3(Input.mousePosition.x, 940.0f, distance);
-		else
-			mousePos = new Vector3(940.0f, Input.mousePosition.y, distance);
-
+		mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
 
 		Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
-		transform.position = objPos;
+
+		if (isHorizontal == true)
+		{
+			transform.position = new Vector3(objPos.x, transform.position.y, transform.position.z);
+
+			if (transform.position.x > maxX)
+				transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+			else if (transform.position.x <minX)
+				transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+		}
+		else
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, objPos.y);
+		}
 	}
 
 }
