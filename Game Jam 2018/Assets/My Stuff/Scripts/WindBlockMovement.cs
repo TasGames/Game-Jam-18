458436dddc9f28bff2072ef9918fb4e22c161bda
Thomas Simon <thomas.simon@btopenworld.com>
2018-11-09 14:05:14 +0000

    Mouse can drag block

---
+++ Game Jam 2018/Assets/My Stuff/Scripts/WindBlockMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindBlockMovement : MonoBehaviour 
+{
+	[SerializeField] protected bool isHorizontal;
+	[SerializeField] protected float movementRange;
+
+	float distance = 28.0f;
+
+	void Start()
+	 {
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 mousePos;
+		//Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+
+		if (isHorizontal == true)
+			mousePos = new Vector3(Input.mousePosition.x, 940.0f, distance);
+		else
+			mousePos = new Vector3(940.0f, Input.mousePosition.y, distance);
+
+
+		Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
+		transform.position = objPos;
+	}
+
+}
